395b15d90af81483e8f7806f43435ef7524415fb
carlosflunauc <90459421+carlosflunauc@users.noreply.github.com>
2021-11-08 19:57:20 -0500

    create migration 3 and metodos repositorio paciente (add, getall)
    Signed-off-by: carlosflunauc <90459421+carlosflunauc@users.noreply.github.com>

--- Hospital.App.Consola/Program.cs
+++ Hospital.App.Consola/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using Hospital.App.Dominio;
 using Hospital.App.Persistencia;
 
@@ -6,9 +7,35 @@ namespace Hospital.App.Consola
 {
     class Program
     {
+        private static IRepositorioPaciente _repoPaciente = new RepositorioPaciente();
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Resultado de la Consulta: ");
+
+            //AddPaciente();
+            MostrarPacientes();
+        }
+        private static void AddPaciente()
+        {
+            var paciente = new Paciente
+            {
+                Nombre = "Pepito",
+                Apellidos = "Perez",
+                NumeroTelefono = "6068848",
+                Genero = Genero.Otro,
+                Direccion = "Calle 4 No 6-9",
+                Ciudad = "Manizales",
+                FechaNacimiento = new DateTime(1985,05,19)
+            };
+            _repoPaciente.AddPaciente(paciente);
+        }
+        private static void MostrarPacientes()
+        {
+            IEnumerable<Paciente> pacientes = _repoPaciente.GetAllPacientes();
+            foreach (var paciente in pacientes)
+            {
+                Console.WriteLine(paciente.Nombre+" "+paciente.Apellidos);
+            }
         }
     }
 }
